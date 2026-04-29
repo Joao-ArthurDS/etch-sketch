307fa4d7c799caf25f5807593d53279d801f1e1e
Joao <146687118+Joao-ArthurDS@users.noreply.github.com>
2026-04-29 17:16:04 -0300

    rendering the grid full functioning with 16x16

--- index.js
+++ index.js
@@ -6,19 +6,27 @@ x = 16;
 grid.style['display'] = 'flex';
 grid.style['flex-wrap'] = 'wrap';
 
-const newWidth = (window.innerWidth / x) -2; //minus 2 to set the border at 1px
-const newHeight = (window.innerHeight / x) -2;
-
-for (let i = 0; i < x; i++){
-    for (let j = 0; j < x; j++){
-        const div = document.createElement('div');
-        div.classList.add('square');
-
-        div.style.width = `${newWidth}px`;
-        div.style.height = `${newHeight}px`;
-        div.innerHTML = `${i} - ${j}`
-        grid.appendChild(div);
-    }
+if (grid.childElementCount > 0){
+    window.addEventListener('resize', resizeElement); //maybe
+} else {
+  resizeElement();  
 }
 
-//window.addEventListener('resize', resizeElement); //maybe
+function resizeElement(){
+    grid.replaceChildren();
+
+    const newWidth = (grid.clientWidth / x) -2; //minus 2 to set the border at 1px
+    const newHeight = (grid.clientHeight / x) -2;
+
+    for (let i = 0; i < x; i++){
+        for (let j = 0; j < x; j++){
+            const div = document.createElement('div');
+            div.classList.add('box');
+
+            div.style.width = `${newWidth}px`;
+            div.style.height = `${newHeight}px`;
+            div.innerHTML = `${i} - ${j}`
+            grid.appendChild(div);
+        }
+    }
+}
