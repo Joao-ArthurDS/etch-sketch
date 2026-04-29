b642e891bedf15797c780cef98e3000bb83ea7ca
Joao <146687118+Joao-ArthurDS@users.noreply.github.com>
2026-04-29 12:34:10 -0300

    Placed the button on the screen and adjusting the grid drawing

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 let x;
-const grid = document.querySelector('div.grid');
+const grid = document.querySelector('div#grid');
+
+x = 16;
 
 grid.style['display'] = 'flex';
 grid.style['flex-wrap'] = 'wrap';
 
-for (let i = 0; i <= 16; i++){
-    const div = document.createElement('div');
-    
-    grid.appendChild(div);
+const newWidth = (window.innerWidth / x) -2; //minus 2 to set the border at 1px
+const newHeight = (window.innerHeight / x) -2;
+
+for (let i = 0; i < x; i++){
+    for (let j = 0; j < x; j++){
+        const div = document.createElement('div');
+        div.classList.add('square');
+
+        div.style.width = `${newWidth}px`;
+        div.style.height = `${newHeight}px`;
+        div.innerHTML = `${i} - ${j}`
+        grid.appendChild(div);
+    }
 }
+
+//window.addEventListener('resize', resizeElement); //maybe
